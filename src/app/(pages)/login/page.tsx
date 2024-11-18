124cec0337ce0bb19402c589843652463a7d2cfb
mucahitcetin <mucahitcetin01@gmail.com>
2024-11-19 02:14:44 +0300

    types fixed

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { login } from "@/app/services/auth";
+import { login } from "@/app/api";
 import Button from "@/app/components/Button";
 import { useRouter } from "next/navigation";
 
--- src/app/services/cart.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const addToCart = async (cartId: number, products: any[]) => {
-  const response = await axios.put(
-    `https://dummyjson.com/carts/${cartId}`,
-    { products }
-  );
-  return response.data;
-};
